640dc7a70f286c4dd73622bcfed8b801f1617329
pink-27 <vihanvashishth2712@gmail.com>
2025-04-10 11:06:03 +0530

    added couple fixes and a multithread approach

--- src/main/java/org/example/server/Server.java
+++ src/main/java/org/example/server/Server.java
@@ -1,7 +1,6 @@
 package org.example.server;
 
 import org.example.Store.inMemoryStore;
-import org.json.JSONObject;
 
 import java.io.*;
 import java.net.ServerSocket;
@@ -15,9 +14,9 @@ public class Server {
     private BufferedReader reader;
     private BufferedWriter writer;
     public static int PORT = 3001;
-    private String STOP="##";
+    private String STOP = "##";
 
-    public Server(){
+    public Server() {
         try {
             serverSocket = new ServerSocket(PORT);
             store = new inMemoryStore();
@@ -26,52 +25,18 @@ public class Server {
         }
     }
 
-    public void handleClient(){
+    public void handleClient() {
         try {
-            while(true) {
+            int clientID=0;
+            while (true) {
                 clientSocket = serverSocket.accept();
-                reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-                readMessages();
+                MultiThreadServer threadServer = new MultiThreadServer(serverSocket, clientSocket, store, ++clientID);
+                threadServer.start();
             }
-
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private void readMessages() throws IOException {
-        String line;
-        while((line = reader.readLine()) != null && !line.equals(STOP)){
-            JSONObject json = new JSONObject(line);
-            if (json.has("value")) {
-                String key = (String) json.get("key");
-                String value = (String) json.get("value");
-                updateKeyValue(key, value);
-            } else {
-                String key = (String) json.get("key");
-                getValue(key);
-            }
-        }
-    }
 
-    public void getValue(String Key) throws IOException {
-        String value = store.getValue(Key);
-        JSONObject response = new JSONObject();
-        response.put("key", Key);
-        response.put("value", value != null ? value : JSONObject.NULL);
-        if(value==null){
-            response.put("status","error");
-        }
-        else{
-            response.put("status","ok");
-        }
-        writer.write(response.toString());
-        writer.newLine();
-        writer.flush();
-
-    }
-    public void updateKeyValue(String Key, String Value){
-        store.updateKeyVal(Key,Value);
-    }
 }
